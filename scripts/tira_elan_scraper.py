96637c4101b1e194da973a7cc06ecc339eefd3b7
Mark Simmons <marks21@utexas.edu>
2025-08-18 09:09:06 -0400

    move docstr to top

--- scripts/tira_elan_scraper.py
+++ scripts/tira_elan_scraper.py
@@ -1,13 +1,13 @@
+"""
+Reads .eaf files from `TIRA_RECORDINGS_GDRIVE` and makes a list stored in `meta/tira_elan_raw.csv`
+with file basenames, timestamps, Elan tier names and values.
+"""
+
 import os
 from glob import glob
 from pympi import Eaf
 from tqdm import tqdm
 import pandas as pd
-
-"""
-Reads .eaf files from `TIRA_RECORDINGS_GDRIVE` and makes a list stored in `meta/tira_elan_raw.csv`
-with file basenames, timestamps, Elan tier names and values.
-"""
 
 TIRA_RECORDINGS_GDRIVE = os.environ.get("TIRA_RECORDINGS_GDRIVE")
 DATA_DIR = os.environ.get(
