69bd502ae3148277eba832ccbbf3f0b2ce721770
Mark Simmons <marks21@utexas.edu>
2025-08-22 07:04:57 -0700

    write function for exporting verb roots and senses from Excel to csv

--- scripts/lexicon_builder.py
+++ scripts/lexicon_builder.py
@@ -2,3 +2,22 @@
 TODO: Load lexical data from Excel spreadsheets and save as .csv files
 containing data for a lexeme's root, principal parts/inflection class, and gloss.
 """
+
+import pandas as pd
+import os
+from dataset_builder import DATA_DIR, EXCEL_VERBS_PATH, EXCEL_SHEET_NAME
+
+OUTPATH = os.path.join(DATA_DIR, "verb_roots.csv")
+
+def get_roots_from_excel() -> int:
+    df = pd.read_excel(EXCEL_VERBS_PATH, sheet_name=EXCEL_SHEET_NAME)
+    get_translation_and_class = lambda row: f"{row['Translation']} ({row['Inflection class']})"
+    df['sense']=df.apply(get_translation_and_class, axis=1)
+    group_by_root = df.groupby(by='Root')
+    root2unique_senses = group_by_root['sense'].apply(set).apply("; ".join)
+    root2unique_senses.to_csv(OUTPATH, index_label='root')
+
+    return 0
+
+if __name__ == '__main__':
+    get_roots_from_excel()
