f97b0fefb0ff62246e5ecccbaddc82e92c3e5828
Mark Simmons <marks21@utexas.edu>
2025-08-22 13:15:30 -0700

    add boilerplate

--- src/lexicon.py
+++ src/lexicon.py
@@ -1,4 +1,15 @@
 """
-TODO: Script that loads lexical data from .csv files and compiles FSTs mapping
+WIP: Script that loads lexical data from .csv files and compiles FSTs mapping
 stems to glosses and to principal parts.
 """
+
+import pynini
+
+VERB_ROOTS_PATH = 'data/verb_roots_new.csv'
+
+def main() -> int:
+
+    return 0
+
+if __name__ == '__main__':
+    main()
