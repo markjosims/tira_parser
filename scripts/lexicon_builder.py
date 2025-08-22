de8f60cbdf51e73a5e66afa9fa2cbc1611e13049
Mark Simmons <marks21@utexas.edu>
2025-08-22 13:02:24 -0700

    ingest cleaned verb roots into excel spreadsheet

--- scripts/lexicon_builder.py
+++ scripts/lexicon_builder.py
@@ -10,19 +10,63 @@ from dataset_builder import DATA_DIR, EXCEL_VERBS_PATH, EXCEL_SHEET_NAME
 ROOTS_OUTPATH = os.path.join(DATA_DIR, "verb_roots.csv")
 ROOTS_INPATH = os.path.join(DATA_DIR, "verb_roots_new.csv")
 
+OLD_ROOT_COL = 'Root'
+OLD_SENSE_COL = 'Translation'
+OLD_FV_COL = 'Inflection class'
+
+ROOT_COL = 'verb_root'
+SENSE_COL = 'root_sense'
+FV_COL = 'root_fv'
+
 def get_roots_from_excel() -> int:
     """
     Creates a `.csv` file from Excel data containing a single row for each unique verb root
     along with all senses and inflection classes associated with that root.
     """
     df = pd.read_excel(EXCEL_VERBS_PATH, sheet_name=EXCEL_SHEET_NAME)
-    get_translation_and_class = lambda row: f"{row['Translation']} ({row['Inflection class']})"
-    df['sense']=df.apply(get_translation_and_class, axis=1)
-    group_by_root = df.groupby(by='Root')
-    root2unique_senses = group_by_root['sense'].apply(set).apply("; ".join)
+    get_translation_and_class = lambda row: f"{row[OLD_SENSE_COL]} ({row[OLD_FV_COL]})"
+    df['old_sense']=df.apply(get_translation_and_class, axis=1)
+    group_by_root = df.groupby(by=OLD_ROOT_COL)
+    root2unique_senses = group_by_root['old_sense'].apply(set).apply("; ".join)
+    root2unique_senses['new_sense']=''
+    root2unique_senses['merge_with']=''
     root2unique_senses.to_csv(ROOTS_OUTPATH, index_label='root')
 
     return 0
 
+def apply_roots_to_excel() -> int:
+    """
+    Loads data from `ROOTS_INPATH` and updates `root_sense` and `root_fv_class` columns
+    in Excel data.
+    """
+    excel_df = pd.read_excel(EXCEL_VERBS_PATH, sheet_name=EXCEL_SHEET_NAME)
+    roots_df = pd.read_csv(ROOTS_INPATH)
+
+    rows_to_merge = ~roots_df['merge_with'].isna()
+    root2merge = {}
+    roots_df[rows_to_merge].apply(
+        lambda row: root2merge.update({row['root']: row['merge_with']}),
+        axis=1,
+    )
+    roots_df[~rows_to_merge].apply(
+        lambda row: root2merge.update({row['root']: row['root']}),
+        axis=1,
+    )
+    root2sense = {}
+    roots_df[~rows_to_merge].apply(
+        lambda row: root2sense.update({row['root']: row['new_sense']}),
+        axis=1,
+    )
+    
+    print("Merging roots in Excel data...")
+    excel_df[ROOT_COL]=excel_df[OLD_ROOT_COL].map(root2merge)
+    print("Adding senses to Excel data...")
+    excel_df[SENSE_COL]=excel_df[ROOT_COL].map(root2sense)
+
+    excel_df.to_excel(EXCEL_VERBS_PATH, sheet_name=EXCEL_SHEET_NAME)
+
+    return 0
+
 if __name__ == '__main__':
-    get_roots_from_excel()
+    # get_roots_from_excel()
+    apply_roots_to_excel()
