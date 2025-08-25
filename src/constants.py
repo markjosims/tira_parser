1e035848d5246fc5d3fa2ddc12049a6bc7786640
Mark Simmons <marks21@utexas.edu>
2025-08-25 06:40:42 -0700

    add filepaths

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+import os
+
+VERB_ROOTS_PATH = 'data/verb_roots_final.csv'
+
+FST_DIR = "fst/"
+ROOT2GLOSS_FST_PATH = os.path.join(FST_DIR, "root2gloss.fst")
+ROOT2FV_FST_PATH = os.path.join(FST_DIR, "root2fv.fst")
