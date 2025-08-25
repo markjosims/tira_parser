db6a3c9e065725832f56da57bdc1ac1d7167a267
Mark Simmons <marks21@utexas.edu>
2025-08-25 06:41:13 -0700

    save lexical transducers for verbs

--- src/lexicon.py
+++ src/lexicon.py
@@ -4,10 +4,33 @@ stems to glosses and to principal parts.
 """
 
 import pynini
+import pandas as pd
+from constants import VERB_ROOTS_PATH, ROOT2FV_FST_PATH, ROOT2GLOSS_FST_PATH
 
-VERB_ROOTS_PATH = 'data/verb_roots_new.csv'
+def get_root2gloss_fst() -> pynini.Fst:
+    verbs_df = pd.read_csv(VERB_ROOTS_PATH)
+    root2gloss_strs = [list(t) for t in zip(
+        verbs_df['verb_root'].tolist(),
+        verbs_df['root_sense'].tolist())
+    ]
+    root2gloss = pynini.string_map(root2gloss_strs)
+    return root2gloss
+
+def get_root2fv_fst() -> pynini.Fst:
+    verbs_df = pd.read_csv(VERB_ROOTS_PATH)
+    root2fv_strs = [list(t) for t in zip(
+        verbs_df['verb_root'].tolist(),
+        verbs_df['root_fv'].tolist())
+    ]
+    root2fv = pynini.string_map(root2fv_strs)
+    return root2fv
 
 def main() -> int:
+    root2gloss = get_root2gloss_fst()
+    root2gloss.write(ROOT2GLOSS_FST_PATH)
+
+    root2fv = get_root2fv_fst()
+    root2fv.write(ROOT2FV_FST_PATH)
 
     return 0
 
